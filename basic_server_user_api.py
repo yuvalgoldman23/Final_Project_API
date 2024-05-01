82c53770e0e2521067659c6046b880c2717d858c
Yuval Goldman <63667515+yuvalgoldman23@users.noreply.github.com>
2024-05-01 20:21:39 +0300

    Update basic_server_user_api.py
    fixed streaming providers api

--- basic_server_user_api.py
+++ basic_server_user_api.py
@@ -291,7 +291,7 @@ def create_post(token_info):
     # Create post object
     new_post = {
         "text": data['text'],
-        "mentioned_id": data.get['mentioned_id'],
+        "mentioned_id": data['mentioned_id'],
         "user_id": user_id
     }
     # Save post to database or perform further actions
@@ -330,35 +330,43 @@ def get_last_20_posts_mentioning_content_id(content_id):
 
 
 # TODO Replace with your TMDB API key
-TMDB_API_KEY = "YOUR_TMDB_API_KEY"
+TMDB_API_KEY = 'f3ca6c11eaf43465a1814fa3a2440f37'
 
 
+# TODO remove territory from this request?
 @app.route('/api/streaming-providers', methods=['GET'])
 def get_streaming_providers():
     content_id = request.args.get('content_id')
-    territory = request.args.get('territory')
+    territory = request.args.get('territory', None)
     # Content type is 'tv' or 'movie'
     content_type = request.args.get('content_type')
-    if not content_id or not territory:
+    if not content_id:
         return jsonify({"error": "Content ID and territory are required"}), 400
-
     # Fetch streaming providers from TMDB
     tmdb_url = f"https://api.themoviedb.org/3/{content_type}/{content_id}/watch/providers"
     headers = {
-        "Authorization": f"Bearer {TMDB_API_KEY}",
+        "api_key": f"{TMDB_API_KEY}",
         "accept": "application/json"
     }
-    params = {
-        "territory": territory
-    }
-
+    params = {"api_key": TMDB_API_KEY}
     try:
-        response = requests.get(tmdb_url, headers=headers, params=params)
+        response = requests.get(tmdb_url, params=params)
         response.raise_for_status()
         data = response.json()
+        # 'flatrate' means "available on streaming"
         streaming_providers = data.get("results", {}).get(territory, {}).get("flatrate", [])
-        provider_names = [provider.get("provider_name") for provider in streaming_providers]
-        return jsonify({"streaming_providers": provider_names}), 200
+        # TODO - think about what to return if streaming_providers== None due to no flatrate available
+        provider_info = [
+            {
+                "name": provider.get("provider_name"),
+                "logo_path": provider.get("logo_path"),
+                "provider_id": provider.get("provider_id"),
+                "display_priority": provider.get("display_priority")
+            }
+            for provider in streaming_providers
+        ]
+        # todo return logo for a provider or let a different API endpoint do that from our local DB?
+        return jsonify({"providers": provider_info}), 200
     except requests.exceptions.RequestException as e:
         return jsonify({"error": str(e)}), 500
 
