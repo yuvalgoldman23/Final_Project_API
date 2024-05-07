6aa61ecd45a7191892e7e36ce5e7b45ea6b16e18
Me2303 <63667515+Me2303@users.noreply.github.com>
2024-05-07 18:25:08 +0300

    changed port and host binding

--- basic_server_user_api.py
+++ basic_server_user_api.py
@@ -455,4 +455,5 @@ def get_streaming_providers():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
