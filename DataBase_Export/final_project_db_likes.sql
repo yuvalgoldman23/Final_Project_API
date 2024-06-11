aafa81d238e4e83c09fd04c6e26b21c04102418a
dani020799 <46074662+dani020799@users.noreply.github.com>
2024-06-11 11:13:35 +0300

    Chege to User_ID for all tables

--- DataBase_Export/final_project_db_likes.sql
+++ DataBase_Export/final_project_db_likes.sql
@@ -16,30 +16,28 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `reviews`
+-- Table structure for table `likes`
 --
 
-DROP TABLE IF EXISTS `reviews`;
+DROP TABLE IF EXISTS `likes`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `reviews` (
-  `ID` char(12) NOT NULL,
+CREATE TABLE `likes` (
+  `ID` varchar(12) NOT NULL,
   `User_ID` varchar(20) DEFAULT NULL,
-  `TText` varchar(255) DEFAULT NULL,
-  `Parent_ID` char(12) DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `Target_ID` varchar(12) DEFAULT NULL,
+  `Targer_Kind` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `reviews`
+-- Dumping data for table `likes`
 --
 
-LOCK TABLES `reviews` WRITE;
-/*!40000 ALTER TABLE `reviews` DISABLE KEYS */;
-INSERT INTO `reviews` VALUES ('023a2fa3b04a','43534543534','grdrgdr','fdgdrfgdr','2024-06-04 14:15:20'),('04f6fb09717c','43534543534','grdrgdr','fdgdrfgdr','2024-06-04 14:13:57'),('061fa692e695','43534543534','grdrgdr','fdgdrfgdr','2024-06-04 14:20:55'),('43cf94a89388','43534543534','grdrgdr','fdgdrfgdr','2024-06-04 14:19:16'),('4de248c8aad3','43534543534','grdrgdr','fdgdrfgdr','2024-06-04 14:11:06'),('55595e2612fc','43534543534','grdrgdr','fdgdrfgdr','2024-06-04 14:08:10'),('73ab7c8570a1','gergr','gergreg','543543534','2024-05-20 19:40:46'),('75c4e37b2a49','43534543534','grdrgdr','fdgdrfgdr','2024-06-04 14:12:35'),('9b6df38b75ab','43534543534','grdrgdr','fdgdrfgdr','2024-06-04 14:09:56'),('b657b20a1341','43534543534','grdrgdr','fdgdrfgdr','2024-06-04 14:09:19'),('ce5c1ef1eb92','43534543534','grdrgdr','fdgdrfgdr','2024-06-04 14:16:24'),('e6e8fc7410b0','43534543534','grdrgdr','fdgdrfgdr','2024-05-28 13:40:44'),('fbce9341e612','43534543534','grdrgdr','fdgdrfgdr','2024-06-04 14:17:13');
-/*!40000 ALTER TABLE `reviews` ENABLE KEYS */;
+LOCK TABLES `likes` WRITE;
+/*!40000 ALTER TABLE `likes` DISABLE KEYS */;
+/*!40000 ALTER TABLE `likes` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
