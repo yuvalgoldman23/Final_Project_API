c5d0c8271721dc43f1c27f05383929c83dc35246
Me2303 <63667515+Me2303@users.noreply.github.com>
2024-06-22 12:29:56 +0300

    Created a basic, working version of the streaming service recommendation engine. Currently returns the top service covering the watchlist

--- routes/streaming_providers_routes.py
+++ routes/streaming_providers_routes.py
@@ -1,3 +1,5 @@
+import time
+
 from flask import Blueprint, request, jsonify
 from auth import auth_required
 from dotenv import load_dotenv
@@ -13,6 +15,7 @@ streaming_providers_routes = Blueprint('streaming_providers_routes', __name__)
 
 # Fetch TMDB API key from environment variables
 TMDB_API_KEY = os.getenv("TMDB_API_KEY")
+TMDB_ACCESS_TOKEN = os.getenv("TMDB_ACCESS_TOKEN")
 
 
 # TODO add a function to return by  territory?
@@ -40,9 +43,17 @@ def get_streaming_providers(content_id, territory, content_type):
     }
     params = {"api_key": TMDB_API_KEY}
     try:
+        # TODO could we create some DB entries to replace this since the rate is quite limited?
+        # TODO currently i wouldn't display providers in the watchlist page because we might pass the quota?
         response = requests.get(tmdb_url, params=params)
+        # TODO currently sleep if the TMDB api blocks us, find another solution
+        while response.status_code == 429:
+            time.sleep(3)
+            response = requests.get(tmdb_url, params=params)
+        print(f"tmdb url for {content_id} is {tmdb_url}")
         response.raise_for_status()
         data = response.json()
+        print(f"response data for {content_id}", type(data.get("results").get("CA")))
         # 'flatrate' means "available on streaming"
         streaming_providers = data.get("results", {}).get(territory, {}).get("flatrate", [])
         # TODO - think about what to return if streaming_providers== None due to no flatrate available
@@ -66,16 +77,31 @@ def streaming_recommendation():
     if "watchlist_id" not in data:
         return jsonify({"Error": "watchlist_id is required to proceed"}), 404
     watchlist_id = data['watchlist_id']
-    territory = data['territory', 'US']
+    territory = data.get('territory')
+    if not territory:
+        territory = 'US'
     watchlist = get_watchlist_by_id(watchlist_id)
+    print("watchlist", watchlist)
     providers = {}
     for watchlist_object in watchlist:
+        is_movie = watchlist_object.get("is_movie")
+        if is_movie:
+            is_movie = 'movie'
+        else:
+            is_movie = 'tv'
         # TODO determine how to get the territory here?
-        current_providers = get_streaming_providers(watchlist_object["content_id"], territory,  watchlist_object["content_type"])
+        current_providers = get_streaming_providers(watchlist_object["TMDB_ID"], territory,  is_movie)
         for provider in current_providers:
             # Count instances of every streaming provider offering content from our list
-            providers[provider['name']] += 1
+            provider_name = provider["name"]
+            if provider_name not in providers:
+                providers[provider_name] = 1
+            else:
+                providers[provider['name']] += 1
     sorted_providers = sorted(providers.items(), key=lambda item: item[1], reverse=True)
     sorted_providers = OrderedDict(sorted_providers)
-    print(sorted_providers)
+    print("Providers sorted best to worst" , sorted_providers)
+    if len(sorted_providers) == 0:
+        return jsonify({"Error": "No streaming providers found"}), 404
+    return jsonify({"best_provider" : next(iter(sorted_providers.items()))}), 200
     # TODO return max. value in the providers list? limit the list to only well known providers such as hulu, netflix, amazon, hbo , etc?
