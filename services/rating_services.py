dffc99a1aa834e989bde7541b43df9850c634957
dani020799 <46074662+dani020799@users.noreply.github.com>
2024-06-23 18:35:31 +0300

    vdxv

---
+++ services/rating_services.py
@@ -0,0 +1,72 @@
+from database_connector import connection, cursor, cursor2
+import mysql.connector
+from mysql.connector import errorcode
+from flask import jsonify
+
+
+def Add_rating(User_ID, Media_id, rating):
+    try:
+        insert_query = f"INSERT INTO `final_project_db`.`rating`(`User_ID`,`Media_ID`,rating) VALUES (%s,%s,%s,%s) "
+        cursor.execute(insert_query, (User_ID, Media_id, rating))
+        connection.commit()
+        return jsonify({"succses"}), 200
+    except mysql.connector.Error as err:
+
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+
+            print("Something is wrong with your user name or password")
+            return jsonify({"Something is wrong with your user name or password"}), 404
+
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+
+            print("Database does not exist")
+            return jsonify({"Database does not exist"}), 404
+
+        else:
+            print(err)
+            return err,404
+
+
+def get_rating_of_user(user_id):
+
+    try:
+
+         query = f"SELECT * FROM `final_project_db`.`rating` WHERE User_ID = %s"
+         cursor2.execute(query, (user_id,))
+         return jsonify(cursor2.fetchall()),200
+
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print("Something is wrong with your user name or password")
+            return jsonify({"Something is wrong with your user name or password"}), 404
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+            print("Database does not exist")
+            return jsonify({"Database does not exist"}), 404
+
+
+        else:
+            print(err)
+            return err,404
+
+
+def Remove_rating(rating_object_ID, User_ID):
+    try:
+        delete_query = "DELETE FROM `final_project_db`.`reviews` WHERE `ID`= %s AND `User_ID`= %s ;"
+        cursor.execute(delete_query, (rating_object_ID, User_ID))
+        connection.commit()
+        return jsonify({"succses"}), 200
+    except mysql.connector.Error as err:
+
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+
+            print("Something is wrong with your user name or password")
+            return jsonify({"Something is wrong with your user name or password"}), 404
+
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+
+            print("Database does not exist")
+            return jsonify({"Database does not exist"}), 404
+
+        else:
+            print(err)
+            return err,404
